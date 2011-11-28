85e8b5e6741b09a5a40791a4a55b90680b52f6c1
zTrix <i@ztrix.me>
2011-11-28 21:24:55 +0800

    fix read file fail problem, which caused by intr not enabled

--- src/kern-ucore/glue-ucore/entry.c
+++ src/kern-ucore/glue-ucore/entry.c
@@ -44,7 +44,6 @@ __kern_entry(void)
 		fs_init();                  // init fs
 		
 		clock_init();
-		mod_init();
 
 		init_finished = 1;
 		spinlock_release(&init_lock);
@@ -67,6 +66,9 @@ __kern_entry(void)
 	while (init_finished != lcpu_count) ;
 
 	intr_enable();
+	if (lcpu_idx == 0) {
+		mod_init();
+	}
 	cpu_idle();                 // run idle process
 	
 	while (1) ;
